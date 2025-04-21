9abd207b6704723aae40e239618d90bd45d4f13b
Greg <projectyuka86@gmail.com>
2025-04-21 13:36:52 -0400

    second commit

---
+++ Conditional Statements/script.js
@@ -0,0 +1,25 @@
+//  if statement ------
+
+// let temperature = 26;
+
+// if (temperature > 25) {
+//     console.log("it is a hot day");
+// } else console.log("its cold");
+
+//  ----------------------------------------
+
+const marks = 65;
+
+if (marks >= 90) {
+    console.log("Grade: A");
+} else if (marks >= 80) {
+    console.log("Grade: B");
+} else if (marks >= 70) {
+    console.log ("Grade: C");
+} else if (marks >=60) {
+    console.log ("Grade: D");
+} else {
+    console.log ("Grade: F")
+}
+
+// 
