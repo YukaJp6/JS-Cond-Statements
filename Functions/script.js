29203da6efa8b10e8d72da31f431a30011dc3c17
Greg <projectyuka86@gmail.com>
2025-04-24 10:06:05 -0400

    second commit

---
+++ Functions/script.js
@@ -0,0 +1,50 @@
+// function greet() {
+//     console.log("hello, welcome to js");
+// }
+
+// // call the function
+// greet();
+// greet();
+
+// ****************************
+
+// function greetUser(name) {
+//     console.log(`Hello, ${name}`);
+// }
+
+// greetUser("Alice");
+// greetUser("Bob");
+// greetUser("Charles");
+
+// ***************************
+
+// function addNumbers(num1, num2, num3) {
+//     console.log(`Sum: ${num1 + num2 + num3}`);
+// }
+
+// addNumbers(5, 10, 5);
+// addNumbers(10, 10, 2);
+
+// **********************************
+
+// const greet = function(name) {
+//     return `Hello, ${name}`;
+// }
+
+// console.log(greet(`Alice`));
+
+// *************************************
+
+// arrow functions, shorter syntax
+
+// const square = (num) => num * num;
+
+// console.log(square(5));
+
+// //  chatgpt exercise
+
+// const summonGuard = (name) => {
+//     console.log(`${name}, the royal guard has arrived `);
+// }
+
+// summonGuard("Julius")
